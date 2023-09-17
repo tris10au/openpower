ae6a8d45bd1aa9cc0a02ed0e880e4d875eccce9e
Tristan Roberts <tristan_roberts@icloud.com>
2023-09-17 12:35:43 +1000

    make version easier to configure

--- setup.py
+++ setup.py
@@ -2,19 +2,20 @@ from setuptools import setup, find_packages
 
 
 with open("README.md", "r") as f:
-	setup(
-		name="openpower",
-		version="0.1.0",
-		author="tris10au",
-		description="Python library for monitoring and controlling solar power systems, including batteries and inverters",
-		long_description_content_type="text/markdown",
-		long_description=f.read(),
-		url="https://github.com/tris10au/openpower",
-   		packages=find_packages(),
-    	classifiers=[
-        	"Programming Language :: Python :: 3",
-    	    "License :: OSI Approved :: MIT License",
-        	"Operating System :: OS Independent",
-    	],
-    	install_requires=["requests"]
-	)
+	with open("VERSION", "r") as version_file:
+		setup(
+			name="openpower",
+			version=version_file.read().strip(),
+			author="tris10au",
+			description="Python library for monitoring and controlling solar power systems, including batteries and inverters",
+			long_description_content_type="text/markdown",
+			long_description=f.read(),
+			url="https://github.com/tris10au/openpower",
+	   		packages=find_packages(),
+	    	classifiers=[
+	        	"Programming Language :: Python :: 3",
+	    	    "License :: OSI Approved :: MIT License",
+	        	"Operating System :: OS Independent",
+	    	],
+	    	install_requires=["requests"]
+		)
