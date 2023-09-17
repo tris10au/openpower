c1ebb20ffef1f2e9d9b7cfb84c342d70bac2da3c
Tristan Roberts <tristan_roberts@icloud.com>
2023-09-17 12:28:20 +1000

    add CI

--- setup.py
+++ setup.py
@@ -15,5 +15,6 @@ with open("README.md", "r") as f:
         	"Programming Language :: Python :: 3",
     	    "License :: OSI Approved :: MIT License",
         	"Operating System :: OS Independent",
-    	]
+    	],
+    	install_requires=["requests"]
 	)
